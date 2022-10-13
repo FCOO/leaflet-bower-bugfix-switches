0a2120935b384f318b29263885fd6db3fc3eacd8
Niels Holt <niels@steenbuchholt.dk>
2022-10-13 14:38:59 +0200

    moveend-event added to all maps to set center = wrapped inside -90-90, -180-180

--- dist/leaflet-bower-bugfix-switches.js
+++ dist/leaflet-bower-bugfix-switches.js
@@ -7,8 +7,22 @@
 	https://github.com/FCOO
 
 ****************************************************************************/
-(function (/*L, window, document, undefined*/) {
+(function (L /*, window, document, undefined*/) {
 	"use strict";
+
+    /*************************************************
+    Adding onmoveend to adjust map.center.lng to range -180 -> +180
+    *************************************************/
+    function onMoveEnd(){
+        var center      = this.getCenter(),
+            wrapCenter  = this.wrapLatLng( center );
+
+        if ((center.lng != wrapCenter.lng) || (center.lat != wrapCenter.lat))
+            this.setView( wrapCenter, this.getZoom(), {animate: false, reset: true, disableViewprereset: true});
+    }
+    L.Map.addInitHook(function () {
+        this.on('moveend', onMoveEnd);
+    });
 
 
 
